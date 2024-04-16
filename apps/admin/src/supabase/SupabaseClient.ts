f2719939e4c9ccf109c948c884047e8da0933545
Benjamin D. Brodie <digdir@bdbrodie.com>
2024-04-16 16:42:44 +0200

    include dev/test env values inline - verified that this not a safety concern

--- apps/admin/src/supabase/SupabaseClient.ts
+++ apps/admin/src/supabase/SupabaseClient.ts
@@ -3,8 +3,13 @@ import { createClient, SupabaseClient } from "@supabase/supabase-js";
 console.log("Initializing Supabase client with provided URL and Anon Key.");
 
 // Initialize Supabase client
-const supabaseUrl = import.meta.env.VITE_SLACK_APP_SUPABASE_API_URL;
-const supabaseAnonKey = import.meta.env.VITE_SLACK_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = window.location.href === 'https://digdir-assistants-test.gentlemoss-ac9ca445.norwayeast.azurecontainerapps.io/' 
+  ? 'https://ncxehajalbrwbicnzxmn.supabase.co'
+  : import.meta.env.VITE_SLACK_APP_SUPABASE_API_URL;
+  
+const supabaseAnonKey = window.location.href === 'https://digdir-assistants-test.gentlemoss-ac9ca445.norwayeast.azurecontainerapps.io/' 
+  ? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jeGVoYWphbGJyd2JpY256eG1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMyNzE0NTMsImV4cCI6MjAyODg0NzQ1M30.6aEnHNqCnMdaEQl_8iaI7aGIf3XBFTR8BEI7N55vFvU' 
+  : import.meta.env.VITE_SLACK_APP_SUPABASE_ANON_KEY;
 
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
