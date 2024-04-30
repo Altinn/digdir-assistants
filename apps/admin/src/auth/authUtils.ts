98bb14014430cae80f18509342e2a1015bfd2ad9
Benjamin D. Brodie <113984758+bdb-dd@users.noreply.github.com>
2024-04-30 18:14:17 +0200

    Use supabase session handling (#40)

--- apps/admin/src/auth/authUtils.ts
+++ apps/admin/src/auth/authUtils.ts
@@ -1,47 +1,57 @@
-export const getToken = () => {
-  console.log("Retrieving authentication token from local storage.");
-  const slackToken = localStorage.getItem("slackToken");
-  const authToken = localStorage.getItem("authToken");
-  if (slackToken) {
-    console.log("Slack token found.");
-    return slackToken;
-  } else if (authToken) {
-    console.log("Auth token found.");
-    return authToken;
-  } else {
-    console.log("No token found.");
+import { Session } from '@supabase/supabase-js';
+import supabase from "../supabase/SupabaseClient";
+
+export const getToken = async () : Promise<Session | null> => {
+  console.log("Retrieving authentication token from Supabase session.");
+  try {
+    const session = await getSession();
+    if (session && session.access_token) {
+      console.log("getToken(): Token found in Supabase session.");
+      return session;
+    } else {
+      console.log("getToken(): No token found in Supabase session.");
+      return null;
+    }
+  } catch (error) {
+    console.error("getToken(): Error retrieving token from Supabase session.", error);
     return null;
   }
 };
 
-export const isLoggedIn = () => {
-  console.log("Checking if user is logged in.");
-  const token = getToken();
-  if (!token) {
-    console.log("No token found, user is not logged in.");
+export const getSession = async () : Promise<Session | null> => {
+
+  const { data, error } = await supabase.auth.getSession();
+  console.log(`session data:\n${JSON.stringify(data)}`);
+  return data?.session;
+}
+
+export const isLoggedIn = async () => {
+
+  console.log("isLoggedIn(): Checking if user is logged in.");
+  const session = await getToken();
+  if (!session) {
+    console.log("isLoggedIn(): No token found, user is not logged in.");
     return false;
   }
 
   try {
-    const session = JSON.parse(token);
-    const currentTime = Date.now(); // Use Date.now() for UTC time
+    const currentTime = new Date(Date.now()).getTime(); // Use Date.now() for UTC time
+    const expiration = (session.expires_at * 1000);
     // Check if the 'expires_at' field exists in the session object
     if (!session.expires_at) {
-      console.error("Session object does not contain 'expires_at' field.");
+      console.error("isLoggedIn(): Session object does not contain 'expires_at' field.");
       return false;
     }
-    // Convert 'expires_at' to UTC if necessary before comparison
-    const expiryTime = Date.parse(session.expires_at); // Convert 'expires_at' to a timestamp assuming it's in ISO 8601 format
-    if (currentTime > expiryTime) {
-      console.log("Token has expired, logging out user.");
+    if (currentTime > expiration) {      
+      console.log(`isLoggedIn(): Current time: ${currentTime}, Expires at: ${expiration} Token has expired, logging out user.`);
       logout();
       return false;
     }
-    console.log("User is logged in.");
+    console.log("isLoggedIn(): User is logged in.");
     return true;
   } catch (error) {
     console.error(
-      "Error parsing token from local storage:",
+      "isLoggedIn(): Error parsing token from local storage:",
       error.message,
       error.stack,
     );
@@ -53,9 +63,10 @@ export const logout = () => {
   console.log(
     "Logging out user and clearing authentication tokens from local storage.",
   );
-  localStorage.removeItem("slackToken");
-  localStorage.removeItem("authToken");
-  // Redirect to home or login page as needed
-  window.location.href = "/";
-  console.log("User logged out successfully.");
+  supabase.auth.signOut().then(() => {
+    console.log("Supabase auth session ended successfully, redirecting to site root...");
+    window.location.href = "/";
+  }).catch((error) => {
+    console.error("Error logging out of Supabase auth session:", error.message);
+  });
 };
--- apps/admin/src/routes/Routes.tsx
+++ apps/admin/src/routes/Routes.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "../App.jsx";
 import SlackAuth from "../auth/SlackAuth.tsx";
@@ -7,6 +7,22 @@ import { isLoggedIn } from "../auth/authUtils.ts";
 
 const MainRoutes = () => {
   console.log("Initializing routing...");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [authChecked, setAuthChecked] = useState(false);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const authStatus = await isLoggedIn();
+      setIsAuthenticated(authStatus);
+      setAuthChecked(true);
+    };
+
+    checkAuth();
+  }, []);
+
+  if (!authChecked) {
+    return <div>Loading...</div>; // Or any other loading indicator
+  }
 
   return (
     <BrowserRouter>
@@ -14,7 +30,7 @@ const MainRoutes = () => {
         {/* Route for the main application view */}
         <Route
           path="/"
-          element={isLoggedIn() ? <App /> : <Navigate to="/auth/email" />}
+          element={isAuthenticated ? <App /> : <Navigate to="/auth/email" />}
         />
 
         {/* Route for the Slack authentication callback */}
@@ -26,7 +42,7 @@ const MainRoutes = () => {
         {/* Redirect based on authentication status */}
         <Route
           path="*"
-          element={isLoggedIn() ? <Navigate to="/" /> : <Navigate to="/auth" />}
+          element={isAuthenticated ? <Navigate to="/" /> : <Navigate to="/auth" />}
         />
       </Routes>
     </BrowserRouter>
