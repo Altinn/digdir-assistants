e6966aed8cb38add5969653db97083240a71de5b
Benjamin D. Brodie <digdir@bdbrodie.com>
2025-09-18 20:26:08 +0200

    chore: remove deprecated Clojure CLI tools

--- clj-cli/src/typesense/api_config.clj
+++
@@ -1,7 +0,0 @@
-(ns typesense.api-config)
-
-(def typesense-config
-  {:nodes [{:host (System/getenv "TYPESENSE_API_HOST")
-            :port 443
-            :protocol "https"}]
-   :api-key (System/getenv "TYPESENSE_API_KEY_ADMIN")})
